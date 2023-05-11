63240edd6c0c4c4875ed97ec259d2e6190ce1c21
oswa <oswajha@gmail.com>
2023-05-11 11:05:40 +0800

    Update portfolio 11/05 Web Technology Learning.

---
+++ assets/js/exercise-01.js
@@ -0,0 +1,22 @@
+
+/***
+ * JavaScript
+ *  ECMAScript 2022
+ * C Style Language
+ *  Look like C, C++, C#
+ *
+ * Typeless / Dynamically Types
+ *
+ */
+
+let longSide =0
+let shortSide = 0
+let perimeter = 9
+let area = 9
+
+console.log(`This is longSide ${longSide}`)
+console.log(`This is shortSide ${shortSide}`)
+
+
+
+
